dd13ce4a735890dfdd6b7e67d00ad6d50020f932
Alexander Shchegolev <iryoka@gmail.com>
2022-01-16 16:17:09 +0300

    Added a beta version of segmentation1

---
+++ data1.py
@@ -0,0 +1,192 @@
+import os
+from typing import Tuple
+import numpy as np
+import pandas as pd
+import torch, torch.nn
+import torch.nn.functional as NNF
+import torchvision, torchvision.utils, torchvision.transforms
+from torchvision.io.image import ImageReadMode
+import torchvision.transforms.functional as VISIONF
+from tqdm import tqdm as tqdm
+import matplotlib.pyplot as plt
+import math
+from tqdm.notebook import tqdm as tqdm
+
+DEFAULT_OVERLAY_COLORS = ["gray", "green","orange"]
+
+class Dataset(torch.utils.data.Dataset):
+
+    TRAIN_TRANSFORMS = ["randomresizedcrop", "randomflip", "tofloat", "fixlabels"]
+    VAL_TRANSFORMS = ["tofloat", "fixlabels"]
+
+    """
+        transforms may contain the following ones:
+            "randomresizedcrop" -- RandomResizedCrop
+            "tofloat" -- change src dtype to float (leaves label as is)
+            "onehotlabels" -- change label type to class-wise one-hot
+    """
+    def __init__(self, data_dir : str, data_csv : str,  
+        output_size : Tuple[int,int] = (128, 128)) -> None:
+        super().__init__()
+
+        self.transforms = Dataset.TRAIN_TRANSFORMS
+        self.ouput_size = output_size
+        #self.use_transforms = True
+
+        train_datafile = pd.read_csv(os.path.join(data_dir, data_csv), delimiter=",")
+
+        self.train_images = []
+
+        #the dataset is relatively small, so we will store it memory to
+        #avoid HDD latencies
+        for _, row in tqdm(train_datafile.iterrows(), "Loading raw data"):
+            img_src = torchvision.io.read_image(os.path.join(data_dir,row["path_img"]))
+            img_mask = torchvision.io.read_image(os.path.join(data_dir,row["path_msk"]), 
+                ImageReadMode.GRAY)
+            self.train_images.append([img_src, img_mask])
+
+    def train(self):
+        self.transforms = Dataset.TRAIN_TRANSFORMS
+    
+    def eval(self):
+        self.transforms = Dataset.VAL_TRANSFORMS
+
+    def __len__(self):
+        return len(self.train_images)
+
+    def __getitem__(self, index):
+        img, label = self.train_images[index]
+
+        for transform in self.transforms:
+            if (transform == "randomresizedcrop"):
+                #TODO: magic constants! 
+                scale=(0.08, 1.0)
+                ratio=(3. / 4., 4. / 3.)
+
+                i, j, h, w = torchvision.transforms.RandomResizedCrop.get_params(img, scale, ratio)
+
+                img = VISIONF.resized_crop(img, i, j, h, w, self.ouput_size)
+                label = VISIONF.resized_crop(label, i, j, h, w, self.ouput_size, 
+                    interpolation=VISIONF.InterpolationMode.NEAREST)
+            elif (transform == "tofloat"):
+                img = torchvision.transforms.ConvertImageDtype(torch.float)(img)
+            elif (transform == "fixlabels"): 
+                label = label.div(127, rounding_mode="floor").squeeze()
+            elif (transform == "randomflip"):
+                if torch.rand(1) < 0.5:
+                    img = VISIONF.vflip(img)         
+                    label = VISIONF.vflip(label)         
+                if torch.rand(1) < 0.5:
+                    img = VISIONF.hflip(img)   
+                    label = VISIONF.hflip(label)               
+            elif (transform == "jitter"):
+                img = torchvision.transforms.ColorJitter(0.2, 0.2, 0.2, 0.1)(img)
+            else:
+                print(f"Warning: unknown transorm ignored: {transform}")
+
+        return img, label
+
+def show(imgs, labels=[]):
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+    plt.figure(dpi=60)
+    _, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = torchvision.transforms.functional.to_pil_image(img)
+        axs[0, i].imshow(np.asarray(img))
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+        if labels:
+            axs[0,i].set_title(labels[i])
+        plt.savefig('filename.png', dpi=1200)
+
+def draw_prediction(src, label, model, colors = DEFAULT_OVERLAY_COLORS, 
+        device = torch.device("cpu"), precomputed_prediction_classes = None):
+    prediction_class = precomputed_prediction_classes
+    if prediction_class is None:        
+        src_cuda = src
+        if len(src.shape) == 3: src_cuda = src.unsqueeze(0)
+
+        src_cuda = src_cuda.to(device)
+
+        #print(device)
+        #print(src_cuda.device)
+        prediction = model(src_cuda).detach()
+        prediction_class = torch.argmax(prediction, dim = 1).to(torch.int64)
+    
+    src = torchvision.transforms.ConvertImageDtype(torch.uint8)(src)
+
+    prediction_masks = NNF.one_hot(prediction_class, num_classes = 3)
+    if prediction_masks.dim() == 3: 
+        prediction_masks = prediction_masks.unsqueeze(0)
+    print(f"Prediction mask shape: {prediction_masks.shape}")
+    prediction_masks = prediction_masks.transpose(0, 3).to(torch.bool).squeeze()
+
+    # print(src.shape, prediction_class.shape, prediction_masks.shape, label.shape)
+    dst = torchvision.utils.draw_segmentation_masks(src, prediction_masks,
+        colors=colors, alpha=0.3)
+    #print(prediction_class)
+    show([src, label, prediction_class.squeeze().to(torch.uint8), dst],
+        ["src", "label", "predicted", "overlay"])
+
+# def predict_by_patches(src, tile_size : Tuple[int, int], 
+#         model : torch.nn.Module, use_softmax : bool,
+#         device = "cpu"):
+#     h, w = src.shape[-2:]
+#     tileh, tilew = tile_size
+#     assert tilew % 2 == 0 and tileh %2 == 0, "so far we only support even tile sizes"
+#     pad_left, pad_right, pad_top, pad_bottom = 0, 0, 0, 0
+#     if w % tilew != 0:
+#         #pad horizontally
+#         padw = tilew * int(math.ceil(w / tilew)) - w
+#         pad_left = padw // 2
+#         pad_right = padw - pad_left
+#     if h % tileh != 0:
+#         #pad vertically
+#         padh = tileh * int(math.ceil(h / tileh)) - h
+#         pad_top = padh // 2
+#         pad_bottom = padh - pad_top
+#     padded_src = torchvision.transforms.Pad([pad_left, pad_top, pad_right, pad_bottom], padding_mode="reflect")(src)
+#     paddedw = w + pad_left + pad_right
+#     paddedh = h + pad_top + pad_bottom
+    
+#     result = torch.zeros_like(padded_src)
+
+#     #number of steps of considering a tile
+#     wsteps = 2 * (paddedw // tilew) - 1
+#     hsteps = 2 * (paddedh // tileh) - 1
+
+#     print(f"Padding input: {src.shape}, output: {padded_src.shape}, wsteps: {wsteps}, hsteps: {hsteps}")
+#     # return
+    
+#     half_tileh = tileh // 2
+#     half_tilew = tilew // 2
+
+#     tile = None #TODO:????
+#     tile_prediction = None
+
+#     for wpos in tqdm(range(wsteps)):
+#         for hpos in range(hsteps):
+#             tile = padded_src[:,hpos * half_tileh : hpos * half_tileh + tileh, wpos * half_tilew : wpos * half_tilew + tilew]
+#             # print("Iteration start")
+#             #print(f"Tile shape: {tile.shape}")
+#             #torch.cuda.empty_cache() 
+#             tile_prediction = model(tile.unsqueeze(0).to(device)).cpu().squeeze()
+#             #TODO:should we use raw logits?
+#             # print(tile.shape, tile_prediction.shape)
+#             if use_softmax:
+#                 tile_prediction = NNF.softmax(tile_prediction, dim = 0)
+#             # print(tile_prediction.shape)
+#             result[:,hpos * half_tileh : hpos * half_tileh + tileh, wpos * half_tilew : wpos * half_tilew + tilew] = \
+#                 result[:,hpos * half_tileh : hpos * half_tileh + tileh, wpos * half_tilew : wpos * half_tilew + tilew] + \
+#                 tile_prediction
+
+#     result = torch.argmax(result, dim = 0)
+#     print(f"PRE-crop result shape {result.shape}")
+
+#     #crop to original size
+#     result = result.squeeze()[pad_top:pad_top+h,pad_left:pad_left+w]
+#     print(f"POST-crop result shape {result.shape}")
+
+#     return result
+
---
+++ modeltools1.py
@@ -0,0 +1,178 @@
+#https://discuss.pytorch.org/t/is-this-a-correct-implementation-for-focal-loss-in-pytorch/43327/8
+# 
+# 
+from cProfile import label
+from pickletools import uint8
+from typing import Tuple
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pytorch_toolbelt.inference.tiles as ptb_tiles#  ImageSlicer, CudaTileMerger, TileMerger
+from torch.utils.data import DataLoader
+import numpy as np
+from tqdm.notebook import tqdm
+import torch.nn.functional as NNF
+import torchvision, torchvision.utils, torchvision.transforms
+import sklearn, sklearn.metrics
+import matplotlib.pyplot as plt
+
+DEFAULT_OVERLAY_COLORS = ["gray", "green","orange"]
+
+class FocalLoss(nn.Module):
+    
+    def __init__(self, weight=None, 
+                 gamma=2., reduction='none'):
+        nn.Module.__init__(self)
+        self.weight = weight
+        self.gamma = gamma
+        self.reduction = reduction
+        
+    def forward(self, input_tensor, target_tensor):
+        log_prob = F.log_softmax(input_tensor, dim=1)
+        prob = torch.exp(log_prob)
+        return F.nll_loss(
+            ((1 - prob) ** self.gamma) * log_prob, 
+            target_tensor, 
+            weight=self.weight,
+            reduction = self.reduction
+        )
+
+'''
+    Provides evaluation on large images via torch toolbelt's tiler
+    
+    model           - torch model to apply. It is forcibly switched to eval mode.
+    image           - image in torchvision float format, channels first
+    batch_size      - size of a batch of tiles to process at once
+    tile_size       - size of a tile (pair)
+    tile_step       - step of tiling. Recommended value: tile_size/2
+    model_device    - a device to send the model and inputs to
+    merger_device   - a device to merge the resulting image on. 
+                    Both device to "cuda" is faster but uses
+                    more VRAM
+
+    Output is an np.ndarray, channels first
+'''
+def tiled_eval(model : torch.nn.Module, image, batch_size, 
+        tile_size : Tuple[int, int], tile_step : Tuple[int, int], 
+        model_device, merger_device) -> np.ndarray:
+    image = image.numpy().transpose(1,2,0)
+    model.eval()
+    model.to(model_device)
+    tiler = ptb_tiles.ImageSlicer(image.shape, 
+        tile_size=tile_size, tile_step=tile_step)
+    tiles = tiler.split(image)
+    # Allocate a CUDA buffer for holding entire mask
+    merger = ptb_tiles.TileMerger(tiler.target_shape, 3, tiler.weight, device = merger_device)
+
+    # Run predictions for tiles and accumulate them
+    for tiles_batch, coords_batch in tqdm(DataLoader(list(zip(tiles, tiler.crops)), 
+            batch_size=batch_size, 
+            num_workers=1, pin_memory=True)):
+        tiles_batch = tiles_batch.to(model_device).permute(0,3,1,2)
+        with torch.no_grad():
+            pred_batch = model(tiles_batch)#.to(merger_device)
+        merger.integrate_batch(pred_batch, coords_batch)
+
+    result = merger.merge().cpu().numpy().transpose(1,2,0)
+    cropped = tiler.crop_to_orignal_size(result).transpose(2,0,1)
+
+    return cropped
+
+def classes_to_masks(img, num_classes = 3):
+    img = img.to(torch.int64)
+    prediction_masks = NNF.one_hot(img, num_classes = num_classes).permute(2, 0, 1).to(torch.bool)
+    return prediction_masks
+
+def logits_to_masks(inputimg, num_classes = 3):
+    #convert to tensor
+    if isinstance(inputimg, np.ndarray):
+        inputimg = torch.tensor(inputimg)
+
+    #convert to channels first
+    if inputimg.size(0) != num_classes:
+        if inputimg.size(2) == num_classes:
+            inputimg = inputimg.permute(2, 0, 1)
+        else:
+            raise ValueError("input must be 3-d with either CF or CL")
+
+    # print(inputimg.shape)
+
+    labels = torch.argmax(inputimg, dim = 0).to(torch.int64)
+
+    # print(labels)
+    # prediction_masks = NNF.one_hot(labels, num_classes = num_classes).permute(2, 0, 1).to(torch.bool)
+    return classes_to_masks(labels, num_classes)
+
+# input: torch, CF
+# color can be None
+def apply_mask(img, masks, colors, alpha = 0.3):
+    img = torchvision.transforms.ConvertImageDtype(torch.uint8)(img)
+    masked = torchvision.utils.draw_segmentation_masks(img, masks, colors=colors, alpha=0.3)
+    return masked
+
+# perdictions must be in mask format; channels first
+def F1_score(prediction, label, class_weights = []):
+    if not class_weights:
+        class_weights = [1] * prediction.shape[0]
+    total_score = 0
+    for channel, weight in zip(range(prediction.shape[0]), class_weights):
+        pred = (prediction[channel,:,:].numpy() > 0).astype(np.uint8).reshape(-1)
+        lab = (label[channel,:,:].numpy() > 0).astype(np.uint8).reshape(-1)
+        score = sklearn.metrics.f1_score(pred, lab) * weight
+        total_score += score
+    return total_score / sum(class_weights)
+
+#no extension 
+def eval_on_dataset(model, dataset, output_filename, tile_batch_size, tile_size, tile_step, model_device, merger_device, class_weights = []):
+    scores = []
+    for i in tqdm(range(len(dataset)), "Images in dataset: "):
+        img, lbl = dataset[i]
+        prediction = tiled_eval(model, img, tile_batch_size, tile_size, tile_step, model_device, merger_device)
+        pred_mask = logits_to_masks(prediction)
+        label_mask = classes_to_masks(lbl)
+        score = F1_score(pred_mask, label_mask, class_weights)
+        print(f"Score: {i}", score)
+        scores.append(score)
+        if output_filename:
+            #compute overlay
+            overlayed_pred = apply_mask(img, pred_mask, DEFAULT_OVERLAY_COLORS, alpha = 0.4)
+            overlayed_label = apply_mask(img, label_mask, DEFAULT_OVERLAY_COLORS, alpha = 0.4)
+            torchvision.io.write_png((img * 255).to(torch.uint8), f"{output_filename}_{i}_1_orig.png")
+            torchvision.io.write_png(((pred_mask > 0) * 127).to(torch.uint8), f"{output_filename}_{i}_2_mask.png")
+            torchvision.io.write_png(overlayed_pred, f"{output_filename}_{i}_3_overlay.png")
+            torchvision.io.write_png(((label_mask > 0) * 127).to(torch.uint8), f"{output_filename}_{i}_4_mask_GT.png")
+            torchvision.io.write_png(overlayed_label, f"{output_filename}_{i}_5_mask_GT_overlay.png")
+
+    score = np.mean(np.array(scores))
+    return score
+
+def train_model(model, loss, optimizer, scheduler, dataloader, num_epochs, device, 
+        print_epoch_summary = True, plot_losses = True):
+    model.train()
+    model.to(device)
+    losses = []
+    for i in tqdm(range(num_epochs)):
+        # print("Epoch: ", i)
+        for batchnum, batch in enumerate(dataloader):
+            # print("Batchnum: ", batchnum)
+            optimizer.zero_grad()
+
+            imgs, labels = batch
+            imgs = imgs.to(device)
+            labels = labels.to(device)
+            dst = model(imgs)
+
+            lossval = loss(dst.to(torch.float), labels.to(torch.long))
+            losscpu = lossval.detach().cpu().numpy()
+            losses.append(losscpu)
+            lossval.backward()
+
+            optimizer.step()
+            scheduler.step()
+
+        if print_epoch_summary:
+            print(f"Epoch {i}, batch {batchnum}, loss: {losscpu}, lr: {scheduler.get_last_lr()}")
+    if plot_losses:                
+        plt.plot(range(len(losses)), losses)
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
